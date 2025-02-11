7cf7f4442b93644ee3bfaabfb50537b6501518ea
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-02-11 05:17:08 -0800

    25-0211-0052 CT Request Done

--- main.ts
+++ main.ts
@@ -11,6 +11,46 @@ function setup_System_AnyLessonAlways_Func () {
 function setup_Student_ThisLessonOnly_Func () {
 	
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    music.play(music.createSoundExpression(WaveShape.Square, 1, 935, 255, 255, 4000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.LoopingInBackground)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    -60,
+    quest_Turn_Duration_Enum.msec_4000,
+    quest_Debug_Show_Enum.Off
+    )
+    music.stopAllSounds()
+    basic.clearScreen()
+})
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Sad)
+    music.play(music.stringPlayable("C D - - - - - - ", 123), music.PlaybackMode.UntilDone)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    0
+    )
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
 function pre_setup_System_AnyLessonAlways_Func () {
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
     "Fix 'show leds' Block's Orientation for Driving"
@@ -33,6 +73,3 @@ quest_Note_1.quest_Show_String_For_Note_Big_Func(
 quest_Note_2.quest_Show_String_For_Note_Big_Func(
 "Student Setup-Code Can Start Below.."
 )
-basic.forever(function () {
-	
-})
