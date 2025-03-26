e3243f273281ceabc870612967afbb1a32b04993
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-03-26 16:50:34 -0700

    robot200-RockPaperScissors-TeacherCode (25-0326-1650, v1.1.0) *** Standardize 'on start' stack

--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
-function setup_Staff_ThisLessonOnly_Func () {
-	
-}
-function setup_System_AnyLessonAlways_Func () {
-    quest_Dashboard.quest_Send_LoginOfBot_ToXrayDashboardOfMb_Func(
-    0,
-    quest_Toggle_OnOff_Enum.On,
-    quest_Debug_Show_Enum.Dashboard_OLED
-    )
-}
-function setup_Student_ThisLessonOnly_Func () {
-	
-}
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . . .
@@ -51,13 +38,6 @@ input.onGesture(Gesture.Shake, function () {
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 	
 })
-function pre_setup_System_AnyLessonAlways_Func () {
-    quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "Fix 'show leds' Block's Orientation for Driving"
-    )
-    display.rotateTo(display.Direction.UpsideDown)
-}
-pre_setup_System_AnyLessonAlways_Func()
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
 "2-Sequence Animation Validates New-Start"
 )
@@ -65,11 +45,16 @@ basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-setup_Staff_ThisLessonOnly_Func()
-setup_System_AnyLessonAlways_Func()
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
+"Setup Code for Teacher:"
+)
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"micro:bit's LED Display is upside-down on bot"
+)
+display.rotateTo(display.Direction.UpsideDown)
 quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "©️ 2025 Quest Institute. All rights reserved."
 )
-quest_Note_2.quest_Show_String_For_Note_Big_Func(
-"Student Setup-Code Can Start Below.."
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
+"Setup Code for Student:"
 )
